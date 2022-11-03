a52b637ebb7ec187778ef01242e003a6c93082ac
dianaostik <dianastud12@gmail.com>
2022-11-03 08:42:47 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+
+// 1 uzdupotis
+console.log("-------- 1 uzduotis ---------");
+
+let arr = [];
+for (let i = 0; i < 10; i++) { 
+    arr[i] = [];
+    for (let a = 0; a < 5; a++) {
+        arr[i][a] = Math.round(5 + Math.random() * 20);        
+    }
+}
+
+console.log(arr);
+
+
+// 2 uzdupotis
+console.log("-------- 2 uzduotis ---------");
